c8fbee3119fcb624da3e749ecadb3db8742de37b
Wellington Teixeira <wellington@tartigrado.com>
2024-11-07 08:10:23 -0300

    add

--- src/app/section-1/section-1.component.ts
+++ src/app/section-1/section-1.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
 import { ImageComponent } from "../components/image/image.component";
-import {VgCoreModule} from '@videogular/ngx-videogular/core';
-import {VgControlsModule} from '@videogular/ngx-videogular/controls';
-import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play';
-import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -12,11 +8,6 @@ import { CommonModule } from '@angular/common';
   imports: [
     CommonModule,
     ImageComponent,
-    VgCoreModule,
-    VgControlsModule,
-    VgOverlayPlayModule,
-    VgBufferingModule,
-    
   ],
   templateUrl: './section-1.component.html',
   styleUrl: './section-1.component.scss'
