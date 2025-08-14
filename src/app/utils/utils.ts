91985d6a6a63ec2776cb42da0980a9ee4346cdda
Wellington Teixeira <wellington@tartigrado.com>
2025-08-14 17:12:58 -0300

    main

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,8 +1,11 @@
 
 export class Utils {
   static downloadPdf(data: Blob, fileName: string): void {
+    const novaAba = window.open('', '_blank');
+
     const blob = new Blob([data], { type: 'application/pdf' });
     const url = window.URL.createObjectURL(blob);
+    novaAba!.location.href = url;
 
     window.open(url);
 
